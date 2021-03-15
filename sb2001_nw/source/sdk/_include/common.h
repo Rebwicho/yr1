aca8a466ff3f55d1dc6a95f963428c77637883c2
rebwicho <rebwicho@gmail.com>
2021-03-15 20:21:09 +0100

    move _include folder location

---
+++ sb2001_nw/source/sdk/_include/common.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define WIN32_LEAN_AND_MEAN 
+#include <Windows.h>
+#undef WIN32_LEAN_AND_MEAN
+
+#include <stdio.h>
+
+#include <thread>
+#include <chrono>
+
+#include <string>
+
+#include <deque>
+
+#include "singleton.h"
+
+typedef unsigned __int8 u8;
+typedef unsigned __int16 u16;
+typedef unsigned __int32 u32;
+
+typedef unsigned long ul32;
+
+#endif // COMMON_H
---
+++ sb2001_nw/source/sdk/_include/sdk.h
@@ -0,0 +1,9 @@
+#ifndef SDK_H
+#define SDK_H
+
+#include "../sdk/utils/console.h"
+
+#include "../sdk/memory/pattern.h"
+#include "../sdk/memory/hooking.h"
+
+#endif // SDK_H
