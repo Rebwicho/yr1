ab555616328c25f29c0eff2baabfd24f3e7e3818
rebwicho <rebwicho@gmail.com>
2021-03-15 20:22:08 +0100

    add singleton

---
+++ sb2001_nw/source/sdk/_include/singleton.h
@@ -0,0 +1,24 @@
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+template<typename owner_type_t>
+class c_singleton
+{
+public:
+	static owner_type_t& get( )
+	{
+		static owner_type_t instance;
+		return instance;
+	}
+
+private:
+	c_singleton( c_singleton&& ) = delete;
+	c_singleton( c_singleton const& ) = delete;
+	c_singleton& operator=( c_singleton const& ) = delete;
+
+protected:
+	c_singleton( ) { }
+	~c_singleton( ) { }	
+};
+
+#endif // SINGLETON_H
