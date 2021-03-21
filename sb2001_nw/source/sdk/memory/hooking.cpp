bd1537519e35d158d484800635d571690fcebd3a
Rebwicho <Rebwicho@gmail.com>
2021-03-21 21:33:15 +0100

    make n_sdk::hooking more static for outside

--- sb2001_nw/source/sdk/memory/hooking.cpp
+++ sb2001_nw/source/sdk/memory/hooking.cpp
@@ -1,59 +1,83 @@
 #include <common.h>
 
 #include <minhook/MinHook.h>
-#pragma comment(lib, "minhook/libMinHook-x86-v141-mdd.lib")
+#pragma comment(lib, "minhook/libMinHook-x86-v142.lib")
 
 #include "hooking.h"
 
-void n_sdk::c_hooking::register_hook( s_hook hook )
+bool n_sdk::c_hooking::minhook_start( )
 {
-	this->m_hook_queue.push_back( hook );
+	return ( MH_Initialize( ) == MH_OK );
+}
+bool n_sdk::c_hooking::minhook_end( )
+{
+	return ( MH_Uninitialize( ) == MH_OK );
 }
 
-u32 n_sdk::c_hooking::hook_all( )
-{
-	for ( u32 i = 0; i < this->m_hook_queue.size( ); i++ )
-		if ( this->m_hook_queue[ i ].execute( ) == 0 ) return i;
-
-	return 0x101;
+n_sdk::hook_t n_sdk::c_hooking::make_hook( const std::string& hook_name, u32 location, abyss hook, abyss original )
+{	
+	return n_sdk::hook_t { hook_name,  ( abyss )location, hook, original };
 }
 
-u32 n_sdk::c_hooking::unhook_all( )
+bool n_sdk::c_hooking::enable( const s_hook& hook )
 {
-	return MH_DisableHook( MH_ALL_HOOKS );
-}
+	// check if hook is valid
+	if ( n_sdk::c_hooking::is_valid( hook ) == 0 )
+		return 0;
 
-bool n_sdk::c_hooking::init_mh( )
-{
-	return MH_Initialize( ) == MH_OK;
-}
-
-bool n_sdk::c_hooking::free_mh( )
-{
-	return MH_Uninitialize( ) == MH_OK;
-}
-
-bool n_sdk::c_hooking::s_hook::execute( )
-{
-	printf( "[ c_memory::hooking ]: hooking %s at %#x ", this->m_hook_name.c_str( ), this->m_func_address );
-
-	auto create_hook = MH_CreateHook( ( void* )this->m_func_address, this->m_func_hook, reinterpret_cast< LPVOID* >( this->m_func_orig ) );
-	if ( create_hook != MH_OK )
+	if ( ( MH_CreateHook( hook.m_location, hook.m_hook, 
+							reinterpret_cast< abyss* >( hook.m_original ) ) != MH_OK )
+		|| ( MH_EnableHook( hook.m_location ) != MH_OK ) )
 	{
-		printf( "failed to create hook\n" );
-
+		//printf( "[ sb2001_nw ]: hooks@failed/function/%s> %#x -> %#x { %#x } &failed\n", 
+		//	hook.m_hook_name.c_str( ), ( u32 )hook.m_location,
+		//	( u32 )hook.m_hook, ( u32 )hook.m_original );
 		return 0;
 	}
 
-	auto enable_hook = MH_EnableHook( ( void* )this->m_func_address );
-	if ( enable_hook != MH_OK )
-	{
-		printf( "failed to enable hook\n" );
-
-		return 0;
-	}
-
-	printf( "done -> f_hook: %#x f_orig: %#x\n", this->m_func_hook, ( void* )this->m_func_orig );
-
+	//printf( "[ sb2001_nw ]: hooks@waiting/function/%s> %#x -> %#x { %#x } &enabled\n",
+	//	hook.m_hook_name.c_str( ), ( u32 )hook.m_location, 
+	//	( u32 )hook.m_hook, ( u32 )hook.m_original );
 	return 1;
+}
+
+bool n_sdk::c_hooking::disable( const s_hook& hook )
+{
+	if ( n_sdk::c_hooking::is_valid( hook ) == 0 )
+		return 0;
+
+	if ( MH_DisableHook( hook.m_location ) != MH_OK ) 
+	{
+		//	printf( "[ sb2001_nw ]: hooks@waiting - hooking...\n" );
+		//printf( "[ sb2001_nw ]: hooks@failed/function/%s> %#x -> %#x { %#x } &failed\n",
+		//	hook.m_hook_name.c_str( ), ( u32 )hook.m_location,
+		//	( u32 )hook.m_hook, ( u32 )hook.m_original );
+		return 0;
+	}
+
+	//printf( "[ sb2001_nw ]: hooks@waiting/function/%s> %#x -> %#x { %#x } &disabled\n",
+	//	hook.m_hook_name.c_str( ), ( u32 )hook.m_location,
+	//	( u32 )hook.m_hook, ( u32 )hook.m_original );
+	return 1;
+}
+
+bool n_sdk::c_hooking::is_valid( const s_hook& hook )
+{
+	// hook is valid when:
+	//  - hook_name != f_null
+	//	- location != 0
+	//	- hook != nullptr
+	//	- original != nullptr
+
+	if ( hook.m_hook_name  == "f_null"
+		|| hook.m_location == nullptr
+		|| hook.m_hook     == nullptr
+		|| hook.m_original == nullptr ) return 0;
+	
+	return 1;
+}
+
+bool n_sdk::c_hooking::do_action( const s_hook& hook, const e_hook_action action )
+{
+	return ( bool ) action ? enable( hook ) : disable( hook );
 }
--- sb2001_nw/source/sdk/memory/hooking.h
+++ sb2001_nw/source/sdk/memory/hooking.h
@@ -8,65 +8,34 @@ namespace n_sdk
 	public:
 		struct s_hook
 		{
-		public:
-			s_hook( ) = delete;
-			s_hook( std::string hook_name, __int32 func_address, void* func_hook, void* original = nullptr )
-				: m_hook_name( hook_name ), m_func_address( func_address ), m_func_hook( func_hook ), m_func_orig( original )
-			{
-				printf( "[ c_memory::hooking ]: registered hook [ %s, %#x ]\n", this->m_hook_name.c_str( ), this->m_func_address );
-			}
-
-		public:
-			bool execute( );
-			/*{
-				printf( "[ c_memory::hooking ]: hooking %s at %#x ", this->m_hook_name.c_str( ), this->m_func_address );
-
-				auto create_hook = MH_CreateHook( ( void* )this->m_func_address, this->m_func_hook, reinterpret_cast< LPVOID* >( this->m_func_orig ) );
-				if ( create_hook != MH_OK )
-				{
-					printf( "failed to create hook\n" );
-
-					return 0;
-				}
-
-				auto enable_hook = MH_EnableHook( ( void* )this->m_func_address );
-				if ( enable_hook != MH_OK )
-				{
-					printf( "failed to enable hook\n" );
-
-					return 0;
-				}
-
-				printf( "done -> f_hook: %#x f_orig: %#x\n", this->m_func_hook, ( void* )this->m_func_orig );
-
-				return 1;
-			}*/
-
-		private:
 			std::string m_hook_name = "f_null";
-
-			__int32 m_func_address = 0;
-
-			void* m_func_hook = nullptr;
-			void* m_func_orig = nullptr;
+			
+			abyss m_location = nullptr;			
+			abyss m_hook = nullptr;
+			abyss m_original = nullptr;
 		};
-		
-	public:
-		// c_hooking( );
-		// ~c_hooking( );
+
+		enum class e_hook_action
+		{
+			disable,
+			enable
+		};
 	
 	public:
-		void register_hook( s_hook hook );
-
-		u32 hook_all( );
-		u32 unhook_all( );
+		static s_hook make_hook( const std::string& hook_name, u32 location, abyss hook, abyss original );
 		
-		bool init_mh( );
-		bool free_mh( );
+		static bool enable( const s_hook& hook );
+		static bool disable( const s_hook& hook );
+		static bool is_valid( const s_hook& hook );
 
-	private:
-		std::deque< s_hook > m_hook_queue = { };
+		static bool do_action( const s_hook& hook, const e_hook_action action );
+		
+		static bool minhook_start( );
+		static bool minhook_end( );
 	};
+
+	typedef n_sdk::c_hooking::s_hook hook_t;
+	typedef n_sdk::c_hooking::e_hook_action hook_action_t;
 }
 
 #endif // HOOKING_H
