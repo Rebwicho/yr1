d4f6bd687e75027772a470baff62a14693a626b8
Rebwicho <Rebwicho@gmail.com>
2021-03-28 00:24:33 +0100

    add core::sessions ( holds current sessions )

---
+++ yr1-server/source/core/server/sessions.h
@@ -0,0 +1,26 @@
+#ifndef SESSIONS_H
+#define SESSIONS_H
+
+namespace core
+{
+	// template< typename type = sdk::c_session >
+	class c_sessions : public c_singleton< core::c_sessions >
+	{
+	public:
+		// needs more stuff xd
+		//template< typename... args_t >
+		//sdk::c_session& emplace_back( args_t&&... args );
+
+		sdk::c_session& add( tcp::socket peer );
+
+		void remove( const u32 sid );
+
+		const u32 size( );
+
+		std::deque< sdk::c_session > m_collection = { };
+	private:
+		std::mutex m_mutex;
+	};
+}
+
+#endif // SESSIONS_H
