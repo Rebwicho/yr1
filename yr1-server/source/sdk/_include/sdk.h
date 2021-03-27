6da553c0798e993277bc6f25f9d64a04d301ace1
Rebwicho <Rebwicho@gmail.com>
2021-03-27 14:54:49 +0100

    test asio lib

---
+++ yr1-server/source/sdk/_include/sdk.h
@@ -0,0 +1,13 @@
+#ifndef SDK_H
+#define SDK_H
+
+//#include "../utils/enum_file.h"
+//#include "../utils/time.h"
+//#include "../utils/basic_log.h"
+//#include "../utils/console.h"
+//
+//
+//#include "../memory/pattern.h"
+//#include "../memory/hooking.h"
+
+#endif // SDK_H
