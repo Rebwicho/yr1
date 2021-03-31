8ea2b3f78ab427339abb0e1379de3575ed4ee6e2
Rebwicho <Rebwicho@gmail.com>
2021-03-31 18:34:27 +0200

    edut exe_main to render our gui for testing

--- sb2001_ldr/source/exe_main.cpp
+++ sb2001_ldr/source/exe_main.cpp
@@ -1,11 +1,9 @@
 #include <common.h>
 #include <sdk.h>
 
-#include "core/loader/injectables.h"
-#include "core/loader/injection.h"
+#include "core/gui/window.h"
 
-int main( int, char* )
-{
+/*
 	n_core::c_injectables::get( ).scan( );
 	std::wcout << "[ sb2001_ldr ]: select injectable: ";
 	u32 selected_injectable;
@@ -18,8 +16,22 @@ int main( int, char* )
 	n_core::c_injection::get( ).set_mode( n_core::injection_mode_t::manualmap );
 	n_core::c_injection::get( ).execute( );
 
+ */
+
+int main( int, char* )
+{
+	if ( n_core::c_window::get( ).create( ) == 0 )
+	{
+		std::cerr << "error: failed to create windows window " << GetLastError( ) << std::endl;
+		//getchar( );
+		//getchar( );
+		return 0;
+	}
+
+	n_core::c_window::get( ).run( );
+
+	//getchar( );
 	getchar( );
-	getchar( );
-	
+
 	return 1;
 }
